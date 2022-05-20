ecdc8c7261275ba43628d43fb3b56f3bc007911c
nabdillaeva <nuriza.abdillaeva.ch@gmail.com>
2022-05-20 11:51:47 +0600

    praktika-11

--- src/main/java/peaksoft/config/AppConfig.java
+++ src/main/java/peaksoft/config/AppConfig.java
@@ -1,9 +1,28 @@
 package peaksoft.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import peaksoft.model.Cat;
+import peaksoft.model.Dog;
+import peaksoft.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "peaksoft")
 public class AppConfig {
+
+    @Bean
+    public Dog dog(){
+        return new Dog();
+    }
+
+    @Bean
+    public Cat cat(){
+        return new Cat();
+    }
+
+    @Bean
+    public Timer timer(){
+        return new Timer();
+    }
 }
--- src/main/java/peaksoft/model/Dog.java
+++ src/main/java/peaksoft/model/Dog.java
@@ -3,14 +3,15 @@ package peaksoft.model;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Cat extends Animal {
+public class Dog extends Animal{
 
-    public Cat(){
+    public Dog(){
 
     }
 
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "Im a Dog";
     }
+
 }
